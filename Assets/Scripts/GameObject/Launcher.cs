34dba300397a2d7d26518f1e78554c1eedaa98f0
Muhammed Uğun <79050510+muhammedugun@users.noreply.github.com>
2024-06-23 15:14:10 +0300

    23.06

--- Assets/Scripts/GameObject/Launcher.cs
+++ Assets/Scripts/GameObject/Launcher.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Launcher : MonoBehaviour
 {
-
     [Header("Ammo")]
     [SerializeField] private GameObject[] ammoTypePrefabs;
     [SerializeField] private Transform ammoSpawnPosition;
@@ -16,7 +15,6 @@ public class Launcher : MonoBehaviour
     [SerializeField] private MMF_Player loadFeedback;
     [SerializeField] private MMF_Player throwFeedback;
 
-    private ThrowInputController _throwInputController;
     private AmmoManager _ammoManager;
     private TextMeshProUGUI _collisionIconText;
     private Vector3 _throwVelocity;
@@ -32,7 +30,6 @@ public class Launcher : MonoBehaviour
     {
         _ammoStartY = ammoSpawnPosition.position.y;
         _ammoManager = FindObjectOfType<AmmoManager>();
-        _throwInputController = FindObjectOfType<ThrowInputController>();
 
         ThrowInputController.Started += Throw;
         _collisionIconText = GameObject.Find("/UI/Canvas/CollisionIcon").GetComponent<TextMeshProUGUI>();
@@ -96,48 +93,82 @@ public class Launcher : MonoBehaviour
     {
         if (CheckAnimStateEmpty())
         {
-            if(_ammoManager.ammunition.Count > 0)
-            {
-                _lastClickTime = Time.time;
-                TriggerLoadAnim();
-                loadFeedback.PlayFeedbacks();
-            }
+            HandleLoadAnimation();
         }
-        else if (_ammoRigidBody.isKinematic && !animator.GetBool("leave") && Time.time > _lastClickTime + 0.4f)
+        else if (CanThrow())
         {
-            loadFeedback.StopFeedbacks();
-            throwFeedback.PlayFeedbacks();
-
-            _collisionIconText.enabled = false;
-
-            _ammo.transform.GetChild(0).GetComponent<TrailRenderer>().enabled = true;
-            _ammo.transform.GetChild(0).GetComponent<Ammo>().throwPos = _ammo.transform.position;
-            
-            if (_lastAmmo!=null && _lastAmmo != _ammo)
-            {
-                if(_lastAmmo.transform.GetChild(0).GetComponent<ExplosiveBase>().isExplode)
-                    Destroy(_lastAmmo.gameObject);
-                else
-                    _lastAmmo.transform.GetChild(0).GetComponent<TrailRenderer>().enabled = false;
-            }
-                
-            _lastAmmo = _ammo;
-
-            animator.SetBool("leave", true);
-            _ammoRigidBody.isKinematic = false;
-            _ammoRigidBody.useGravity = true;
-            _ammoRigidBody.GetComponent<Collider>().enabled = true;
-
-            _height = _ammoRigidBody.transform.position.y - _ammoStartY;
-
-            _ammo.transform.parent = null;
-
-            _ammoRigidBody.AddForce(_ammoRigidBody.transform.right * throwPower * _height, ForceMode.VelocityChange);
-            OnThrowedInvoke();
-
+            ExecuteThrow();
         }
-        
     }
+
+    private void HandleLoadAnimation()
+    {
+        if (_ammoManager.ammunition.Count > 0)
+        {
+            _lastClickTime = Time.time;
+            TriggerLoadAnim();
+            loadFeedback.PlayFeedbacks();
+        }
+    }
+
+    private bool CanThrow()
+    {
+        return _ammoRigidBody.isKinematic && !animator.GetBool("leave") && Time.time > _lastClickTime + 0.4f;
+    }
+
+    private void ExecuteThrow()
+    {
+        loadFeedback.StopFeedbacks();
+        throwFeedback.PlayFeedbacks();
+
+        _collisionIconText.enabled = false;
+
+        EnableTrailRenderer();
+
+        HandlePreviousAmmo();
+
+        SetAmmoProperties();
+
+        ApplyThrowForce();
+
+        OnThrowedInvoke();
+    }
+
+    private void EnableTrailRenderer()
+    {
+        _ammo.transform.GetChild(0).GetComponent<TrailRenderer>().enabled = true;
+        _ammo.transform.GetChild(0).GetComponent<Ammo>().throwPos = _ammo.transform.position;
+    }
+
+    private void HandlePreviousAmmo()
+    {
+        if (_lastAmmo != null && _lastAmmo != _ammo)
+        {
+            if (_lastAmmo.transform.GetChild(0).GetComponent<ExplosiveBase>().isExplode)
+                Destroy(_lastAmmo.gameObject);
+            else
+                _lastAmmo.transform.GetChild(0).GetComponent<TrailRenderer>().enabled = false;
+        }
+        _lastAmmo = _ammo;
+    }
+
+    private void SetAmmoProperties()
+    {
+        animator.SetBool("leave", true);
+        _ammoRigidBody.isKinematic = false;
+        _ammoRigidBody.useGravity = true;
+        _ammoRigidBody.GetComponent<Collider>().enabled = true;
+
+        _height = _ammoRigidBody.transform.position.y - _ammoStartY;
+
+        _ammo.transform.parent = null;
+    }
+
+    private void ApplyThrowForce()
+    {
+        _ammoRigidBody.AddForce(_ammoRigidBody.transform.right * throwPower * _height, ForceMode.VelocityChange);
+    }
+
     private void OnThrowedInvoke()
     {
         EventBus.Publish(EventType.LauncherThrowed);
